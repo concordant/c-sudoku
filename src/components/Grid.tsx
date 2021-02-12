86275cedc0d84bda93309093636e6f88b71da5fb
Yannick Li <yannick.li@concordant.io>
2021-02-12 09:26:49 +0100

    Merge GridUtils to Grid

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -24,7 +24,6 @@
 
 import React from 'react';
 import assert from 'assert';
-import * as GridUtils from './GridUtils';
 import Cell from './Cell';
 import { client } from '@concordant/c-client';
 
@@ -100,7 +99,7 @@ class Grid extends React.Component<IGridProps, IGridState> {
             }
             while(itString.hasNext()) {
                 let val = itString.next()
-                cells[val.first.replace('cell', '')][0] = GridUtils.hashSetToString(val.second)
+                cells[val.first.replace('cell', '')][0] = hashSetToString(val.second)
             }
         })
         this.checkGrid(cells)
@@ -195,7 +194,7 @@ class Grid extends React.Component<IGridProps, IGridState> {
      */
     renderBlock(blockNum: number) {
         assert.ok(blockNum >= 0 && blockNum < 9)
-        let index = GridUtils.blockIndex(blockNum);
+        let index = blockIndex(blockNum);
         return (
             <td>
                 {this.renderCell(index[0])}{this.renderCell(index[1])}{this.renderCell(index[2])}<br />
@@ -211,7 +210,7 @@ class Grid extends React.Component<IGridProps, IGridState> {
     render() {
         return (
             <div className="sudoku">
-                <div><button onClick={this.setMVMap.bind(this, GridUtils.generateStaticGrid())}>Reset</button></div><br />
+                <div><button onClick={this.setMVMap.bind(this, generateStaticGrid())}>Reset</button></div><br />
                 <div><button onClick={() => this.switchConnection()}>{this.state.isConnected ? "Disconnect" : "Connect"}</button></div><br />
                 <table className="grid">
                     <tbody>
@@ -244,7 +243,7 @@ class Grid extends React.Component<IGridProps, IGridState> {
             }
             check[Number(val)-1]++
         }
-        return GridUtils.checkArray(check)
+        return checkArray(check)
     }
 
     /**
@@ -262,7 +261,7 @@ class Grid extends React.Component<IGridProps, IGridState> {
             }
             check[Number(val)-1]++
         }
-        return GridUtils.checkArray(check)
+        return checkArray(check)
     }
 
     /**
@@ -272,7 +271,7 @@ class Grid extends React.Component<IGridProps, IGridState> {
     validateBlock(block: number) {
         assert.ok(block >= 0 && block < 9)
         let check = Array(9).fill(0)
-        let indexList = GridUtils.blockIndex(block)
+        let indexList = blockIndex(block)
         for (let index of indexList) {
             let val = this.state.cells[index][0]
             if (val.length === 0 || val.length > 1) {
@@ -280,7 +279,7 @@ class Grid extends React.Component<IGridProps, IGridState> {
             }
             check[Number(val)-1]++
         }
-        return GridUtils.checkArray(check)
+        return checkArray(check)
     }
 
     /**
@@ -320,7 +319,7 @@ class Grid extends React.Component<IGridProps, IGridState> {
         let indexList : number[] = []
         for (let block = 0; block < 9; block++) {
             if (this.validateBlock(block) === false) {
-                indexList = indexList.concat(GridUtils.blockIndex(block));
+                indexList = indexList.concat(blockIndex(block));
             }
         }
         return indexList
@@ -380,6 +379,78 @@ class Grid extends React.Component<IGridProps, IGridState> {
         }
         this.setState({cells: cells, finished: true})
     }
+}
+
+/**
+ * Return a predefined Sudoku grid as an array.
+ */
+function generateStaticGrid() {
+    const values = ["6", "", "2", "3", "8", "7", "9", "1", "4",
+                    "7", "1", "9", "4", "5", "2", "3", "6", "8",
+                    "3", "4", "8", "1", "9", "6", "2", "5", "7",
+                    "8", "2", "1", "9", "3", "5", "4", "7", "6",
+                    "5", "9", "", "2", "7", "4", "8", "3", "1",
+                    "4", "7", "3", "8", "6", "1", "5", "2", "9",
+                    "1", "8", "7", "5", "2", "9", "", "", "3",
+                    "2", "3", "4", "6", "1", "8", "7", "9", "5",
+                    "", "6", "5", "7", "4", "3", "1", "8", "2"];
+    assert.ok(values.every(x => (x==="" || (Number(x)>=1 && Number(x)<=9))));
+    return values;
+}
+
+/**
+ * Check if a array only contains values 0 or 1.
+ * @param array The array to be checked.
+ */
+function checkArray(array:any) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== 0 && array[i] !== 1) {
+            return false
+        }
+    }
+    return true
+}
+
+/**
+ * Return the position of the first cell of a block.
+ * @param block The block number of which we want the position.
+ */
+function firstCellOfBlock(block:number) {
+    assert.ok(block >= 0 && block < 9)
+    let line = Math.floor(block / 3) * 3
+    let column = (block % 3) * 3
+    return [line,column]
+}
+
+/**
+ * Return an array containing all cell index of a block.
+ * @param block The block number of which we want the cells index.
+ */
+function blockIndex(block: number) {
+    assert.ok(block >= 0 && block < 9)
+    let blocklc = firstCellOfBlock(block)
+    let line = blocklc[0]
+    let col = blocklc[1]
+    let index = [ line      * 9 + col,   line      * 9 + col + 1,  line      * 9 + col + 2,
+                 (line + 1) * 9 + col,  (line + 1) * 9 + col + 1, (line + 1) * 9 + col + 2,
+                 (line + 2) * 9 + col,  (line + 2) * 9 + col + 1, (line + 2) * 9 + col + 2]
+    return index
+}
+
+/**
+ * Concatenates all values of a HashSet as a String.
+ * @param set HashSet to be concatenated.
+ */
+function hashSetToString(set: any) {
+    let res = new Set();
+    let it = set.iterator();
+    while (it.hasNext()) {
+        let val = it.next();
+        if (val !== "") {
+            res.add(val);
+        }
+    }
+    return Array.from(res).sort().join(' ')
 }
 
 export default Grid
